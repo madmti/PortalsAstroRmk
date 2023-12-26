12b220762c1fd7488c2155bb0e39d06dc9f874fe
madmti <supermatrix9@gmail.com>
2023-12-26 01:36:02 -0300

    loads dotenv if env variables wasnt provided

--- src/public.env.ts
+++ src/public.env.ts
@@ -1,3 +1,4 @@
-if (!process.env.BACKEND_URL_REST){ console.log('ERROR GETTING -> process.env.BACKEND_URL') };
+import { config } from "dotenv";
+if (!process.env.BACKEND_URL_REST){ console.log('ERROR GETTING -> process.env.BACKEND_URL'); config(); };
 
 export const BACKEND_URL = import.meta.env.BACKEND_URL_REST || 'ERROR';

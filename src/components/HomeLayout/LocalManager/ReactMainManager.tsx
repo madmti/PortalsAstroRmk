021593c01d298c0391ca4630ae941c3677ebc64a
madmti <supermatrix9@gmail.com>
2023-12-27 13:15:01 -0300

    React local nav section created

--- src/components/HomeLayout/LocalManager/ReactMainManager.tsx
+++ src/components/HomeLayout/LocalManager/ReactMainManager.tsx
@@ -1,4 +1,7 @@
 import { useManagerStore } from '@/store/ManagerStore';
+import UserSection from '@/components/LocalNav/UserSection';
+import MusicSection from '@/components/LocalNav/MusicSection';
+import EventSection from '@/components/LocalNav/EventSection';
 
 export default function ReactMainManager({
 	children,
@@ -10,9 +13,9 @@ export default function ReactMainManager({
 	const position = useManagerStore((store) => store.pos);
 	const Contents = {
 		servers: children,
-		music: 'music',
-		events: 'events',
-		user: 'user',
+		music: <MusicSection />,
+		events: <EventSection />,
+		user: <UserSection />,
 	};
 	return <>{Contents[position] ? Contents[position] : 'Error'}</>;
 }
---
+++ src/components/LocalNav/UserSection.tsx
@@ -0,0 +1,3 @@
+export default function UserSection() {
+	return 'user section';
+}
